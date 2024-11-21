2308b5605a04024d95e98ecf32ad63ddf58adeb0
marcelo-schreiber <marcelorissette15@gmail.com>
2024-11-21 10:06:00 -0300

    Refactor Home component to use dynamic import for WeatherCard and improve code formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-'use server';
+"use server";
 
-import WeatherCard from "@/components/card/latestTempAndHumidity";
 import { Chart } from "@/components/chart/tempAndHumidity";
-import { createClient } from '@/lib/supabase';
+import { createClient } from "@/lib/supabase";
+import dynamic from "next/dynamic";
+const NoSSRWeatherCard = dynamic(
+  () => import("@/components/card/latestTempAndHumidity"),
+  { ssr: false },
+);
 
 export default async function Home() {
   const supabase = await createClient();
 
-  const { data, error } = await supabase.from('weather_view').select('*').order('created_at', { ascending: true });
+  const { data, error } = await supabase
+    .from("weather_view")
+    .select("*")
+    .order("created_at", { ascending: true });
 
   if (error) {
     console.error(error);
@@ -18,9 +25,21 @@ export default async function Home() {
 
   return (
     <main className="flex flex-col justify-center px-10 pt-10">
-      <Chart chartData={data as { created_at: string; temperature: number; humidity: number }[]} />
+      <Chart
+        chartData={
+          data as {
+            created_at: string;
+            temperature: number;
+            humidity: number;
+          }[]
+        }
+      />
       <div className="flex justify-center mt-10">
-      <WeatherCard humidity={latestTempAndHumidity.humidity} temperature={latestTempAndHumidity.temperature} lastUpdated={latestTempAndHumidity.created_at} />
+        <NoSSRWeatherCard
+          humidity={latestTempAndHumidity.humidity}
+          temperature={latestTempAndHumidity.temperature}
+          lastUpdated={latestTempAndHumidity.created_at}
+        />
       </div>
     </main>
   );
